e2d5d672d853de163d6da0a0d4605a48d797358c
ArcticHere <seiergustav@gmail.com>
2025-03-09 17:08:56 +0100

    Update Post.tsx

--- src/components/posts/Post.tsx
+++ src/components/posts/Post.tsx
@@ -61,6 +61,7 @@ export default function Post({ post }: PostProps) {
       </div>
       <Linkify>
         <div className="whitespace-pre-line break-words">{post.content}</div>
+        <div className="whitespace-pre-line break-words">{post.calories}</div>
       </Linkify>
       {!!post.attachments.length && (
         <MediaPreviews attachments={post.attachments} />
