fca0ee1f2d08dc7dab4e8064bfe9234b69451fb0
ArcticHere <seiergustav@gmail.com>
2025-03-09 16:42:59 +0100

    test 2

--- src/components/posts/editor/actions.ts
+++ src/components/posts/editor/actions.ts
@@ -7,17 +7,19 @@ import { createPostSchema } from "@/lib/validation";
 
 export async function submitPost(input: {
   content: string;
+  calories: string;
   mediaIds: string[];
 }) {
   const { user } = await validateRequest();
 
   if (!user) throw new Error("Unauthorized");
 
-  const { content, mediaIds } = createPostSchema.parse(input);
+  const { content, calories, mediaIds } = createPostSchema.parse(input);
 
   const newPost = await prisma.post.create({
     data: {
       content,
+      calories: parseInt(calories),
       userId: user.id,
       attachments: {
         connect: mediaIds.map((id) => ({ id })),
