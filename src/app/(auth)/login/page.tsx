552c80ad31f7e31cd030ddc79167c93226379e58
ArcticHere <seiergustav@gmail.com>
2025-03-09 14:43:30 +0100

    login page change

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -18,11 +18,7 @@ export default function Page() {
           <div className="space-y-5">
             <GoogleSignInButton />
             <div className="flex items-center gap-3">
-              <div className="h-px flex-1 bg-muted" />
-              <span>OR</span>
-              <div className="h-px flex-1 bg-muted" />
             </div>
-            <LoginForm />
             <Link href="/signup" className="block text-center hover:underline">
               Don&apos;t have an account? Sign up
             </Link>
