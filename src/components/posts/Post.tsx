2e2f6153177a40b9ee14b14a0b64787890ffca17
ArcticHere <seiergustav@gmail.com>
2025-04-14 15:15:19 +0200

    Test 2

--- src/components/posts/Post.tsx
+++ src/components/posts/Post.tsx
@@ -36,7 +36,7 @@ export default function Post({ post }: PostProps) {
             <UserTooltip user={post.user}>
               <Link
                 href={`/users/${post.user.username}`}
-                className="block font-bold hover:underline"
+                className="block font-semibold hover:underline"
               >
                 {post.user.displayName}
               </Link>
